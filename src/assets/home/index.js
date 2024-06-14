4f09bdf1e82fc6705c5cf12cd77eb12adb90b9a3
Sprou-t <150780574+Sprou-t@users.noreply.github.com>
2024-06-14 18:02:14 +0800

    partially solved the issue. Need to press ctrl + S to reformat code using eslint

--- src/assets/home/index.js
+++ src/assets/home/index.js
@@ -1,9 +1,8 @@
 import './style.css';
 import './reset.css';
 
-const x = '2';
-const y = 2;
-const a = 1;
-const b = 4;
-const z = '2';
-car ='honda';
+const someFunction = () => {
+  console.log('this is a message');
+};
+
+someFunction();
